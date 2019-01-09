083576c37b81e556307790f2adb181df0b376c54
Alexey Tsaplin-Kupaysinov <info@krdprog.ru>
2019-01-09 13:59:06 +0300

    Create delete-user-and-site.sh

---
+++ delete-user-and-site.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Скрипт принимает 2 переменные: $1 - user $2 - domain
+
+rm -r /var/www/$2 && rm /etc/apache2/sites-available/$2.conf && a2dissite $2.conf && deluser --remove-home $1 && sudo service apache2 r$
+
+# удалим каталог сайта
+# удалим файл конфигурации
+# отключим виртуальный хост
+# удалим юзера
+# перезагрузим апач
